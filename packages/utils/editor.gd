417f9f68b759ad4f85013a268672185cb5ad048d
pomnbhgix <pomnbhgix@outlook.com>
2023-12-05 23:02:42 +0800

    add npc arts

--- packages/utils/editor.gd
+++ packages/utils/editor.gd
@@ -1,10 +1,6 @@
 @tool
 extends Node
 
-var first = true;
-
-func _process(_delta):
-	if first:
-		DisplayServer.window_set_min_size(Vector2i.ZERO)
-		first = false
+func _ready():
+	DisplayServer.window_set_min_size(Vector2i.ZERO)
 
