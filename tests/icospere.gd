cf08e66ad2d7923c2e998ec4b12f7a2281131f31
pomn <pomnbhgix@outlook.com>
2023-12-11 17:41:53 +0800

    update

--- tests/icospere.gd
+++ tests/icospere.gd
@@ -11,6 +11,40 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if update:
-		print_debug("tool test")
+		procedual_mesh()
 		update =  false
 	
+func procedual_mesh():
+	print_debug("procedual mesh")
+	
+	var surface_array = []
+	surface_array.resize(Mesh.ARRAY_MAX)
+
+	# PackedVector**Arrays for mesh construction.
+	var verts = PackedVector3Array()
+	var uvs = PackedVector2Array()
+	var normals = PackedVector3Array()
+	var indices = PackedInt32Array()
+
+	#######################################
+	## Insert code here to generate mesh ##
+	
+	
+	
+	#######################################
+
+	# Assign arrays to surface array.
+	surface_array[Mesh.ARRAY_VERTEX] = verts
+	surface_array[Mesh.ARRAY_TEX_UV] = uvs
+	surface_array[Mesh.ARRAY_NORMAL] = normals
+	surface_array[Mesh.ARRAY_INDEX] = indices
+
+	# Create mesh surface from mesh array.
+	# No blendshapes, lods, or compression used.
+	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_array)
+	
+	
+	
+	
+	pass
+
